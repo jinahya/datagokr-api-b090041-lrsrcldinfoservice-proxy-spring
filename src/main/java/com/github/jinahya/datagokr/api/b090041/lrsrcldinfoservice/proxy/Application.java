05449bf6ae301885923c168eecbea216b27bdd7c
Jin Kwon <onacit@users.noreply.github.com>
2020-12-13 22:41:04 +0900

    updated

---
+++ src/main/java/com/github/jinahya/datagokr/api/b090041/lrsrcldinfoservice/proxy/Application.java
@@ -0,0 +1,12 @@
+package com.github.jinahya.datagokr.api.b090041.lrsrcldinfoservice.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
